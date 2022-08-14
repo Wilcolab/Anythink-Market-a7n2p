d1563d84093f0f826e7f095f293e654da94bb211
nisar20 <107908932+nisar20@users.noreply.github.com>
2022-08-15 00:07:49 +0530

    Update Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -8,7 +8,7 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String,
+    image: {type:String, default:'/frontend/public/placeholder.png'}
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
